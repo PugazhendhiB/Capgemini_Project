035ada5b0adcf28247b314043069dcf8dde76976
Pugazh <90821976+PugazhendhiB@users.noreply.github.com>
2022-01-14 14:31:50 +0530

    Add files via upload

---
+++ Book Management System using SpringBoot+JPA+MySql/BOOKMYSQL/src/main/java/my/BookmysqlApplication.java
@@ -0,0 +1,15 @@
+package my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmysqlApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(BookmysqlApplication.class, args);
+	}
+
+}
