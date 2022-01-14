9a6579589a8ce6beb38b940478357f1f41c6f4fc
Pugazh <90821976+PugazhendhiB@users.noreply.github.com>
2022-01-14 14:32:21 +0530

    Add files via upload

---
+++ CG Contact Query Management System using SpringBoot+JPA+MySql/src/main/java/CG_Contact/ContactquerytestApplication.java
@@ -0,0 +1,13 @@
+package CG_Contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactquerytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactquerytestApplication.class, args);
+	}
+
+}
---
+++ CG Contact Query Management System using SpringBoot+JPA+MySql/src/test/java/CG_Contact_Test/ContactquerytestApplicationTests.java
@@ -0,0 +1,15 @@
+package CG_Contact_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactquerytestApplicationTests {
+
+	@Test
+	void contextLoads() 
+	{
+		
+	}
+
+}
