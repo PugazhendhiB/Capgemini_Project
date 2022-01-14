e6e95ee5ee941c3235635e8a6e3018ea9ed087da
Pugazh <90821976+PugazhendhiB@users.noreply.github.com>
2022-01-14 14:31:25 +0530

    Add files via upload

---
+++ Book Management System using SpringBoot+JPA+H2 Database+Swagger/src/main/java/cgbook/BookRepository.java
@@ -0,0 +1,11 @@
+/*
+ * @Author Pugazh 
+ * JPA Repository are avaliable here
+ */
+package  cgbook;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Integer> {
+
+}
